6f745db0ca75f31d790e03532827c48ecb66865b
Róbert Fuszenecker <robert.fuszenecker@outlook.com>
2018-06-01 21:50:48 +0200

    Added basic elements

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -6,6 +6,10 @@ export interface IProps {
 
 export const TodoApp = ({ name }: IProps): JSX.Element => {
     return (
-        <div>{name}</div>
+        <div>
+            <h1>{name}</h1>
+            <button className="button is-success">Button is-loading</button>
+            <input className="input is-success" type="text" placeholder="Success input" />
+        </div>
     );
 };
